77a7f6b5ccca122bf133bd2e4050fdea076df451
Muhammad Razan Athallah <96050618+athraz@users.noreply.github.com>
2024-06-24 13:53:45 +0700

    add logic to check palindrome

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+stack = []
+
+def push(element: str):
+
+def pop():
+
 def isPalindrome(string: str) -> bool:
-    return 1
+	global stack
+	length = len(string)
+
+	stack = ['0'] * (length + 1)
+
+	mid = length // 2
+	i = 0
+	while i < mid:
+		push(string[i])
+		i += 1
+
+	if length % 2 != 0:
+		i += 1
+
+	while i < length:
+		element = pop()
+		if element != string[i]:
+			return False
+		i += 1
+		
+	return True
 
 if __name__ == "__main__":
     string = input()
